972cb0311724cf0323c8d01006b7639bc116e5ef
Agustín Minetti <minettiagus2@gmail.com>
2026-04-30 10:22:21 -0300

    Realizado ejercicio 3

--- main.js
+++ main.js
@@ -13,3 +13,17 @@ let bonus = 50;
 let penalizacion = 20;
 let puntajeTotal = puntosBase + bonus - penalizacion;
 console.log(puntajeTotal);
+
+const input = document.getElementById("dato");
+const boton = document.getElementById("btn");
+const mensaje = document.getElementById("mensaje");
+
+boton.addEventListener("click", function () {
+ const valor = input.value;
+ const numero = parseFloat(valor);
+ const resultado = numero + 10;
+ mensaje.textContent = "El resultado es: " + resultado;
+ // Convertir valor a número
+ // Sumar 10
+ // Mostrar el resultado en pantalla
+});
